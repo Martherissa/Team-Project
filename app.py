bd2a4f4fbff9eb4352aafa96f1188ecaa781d9b9
Martherissa <zyang3@babson.edu>
2024-04-18 14:05:27 -0400

    trails to add additional pages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template, request, url_for
-from functions import search_recipes_by_ingredient
+from functions import search_recipes_by_ingredient, get_recipet_id, get_recipe_info
 
 
 app = Flask(__name__, template_folder="templates")
@@ -16,10 +16,19 @@ def get_recipe():
 
 
 @app.post("/recipe")
-def nearest_mbta():
+def find_recipe():
     ingredients = request.form["ingredients"]
     recipes = search_recipes_by_ingredient(ingredients)
     return render_template("render_page.html", result=recipes, input=ingredients)
+
+
+@app.post("/recipe")
+def recipe_info():
+    choice = request.get("ChosenRecipe")
+
+    ingredients = request.form["ingredients"]
+    instructions = get_recipe_info(choice, ingredients)
+    return render_template("recipe_info.html", recipeName=choice, detail=instructions)
 
 
 # This handles any 404 error for the website
