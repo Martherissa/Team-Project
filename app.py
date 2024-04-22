d5e54c7463deb0dbf079755ec7f75e8d30d2f08c
Martherissa <zyang3@babson.edu>
2024-04-22 13:23:59 -0400

    Updating more features into the recipe info page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template, request, url_for
-from functions import search_recipes_by_ingredient, get_recipe_steps
+from functions import search_recipes_by_ingredient, get_recipe_steps, get_recipe_image, get_recipe_summary, get_recipe_ingredients
 
 
 app = Flask(__name__, template_folder="templates")
@@ -23,11 +23,15 @@ def find_recipe():
 
 @app.post("/recipe_info")
 def recipe_info():
-    choice = request.form.get("ChosenRecipe")  # Corrected from request.get
+    choice = request.form.get("ChosenRecipe")  
 
     ingredients = request.form["ingredients"]
     instructions = get_recipe_steps(choice, ingredients)
-    return render_template("recipe_info.html", recipeName=choice, detail=instructions)
+    all_ingredients = get_recipe_ingredients(choice, ingredients)
+    images = get_recipe_image(choice, ingredients)
+    summaries = get_recipe_summary(choice, ingredients)
+
+    return render_template("recipe_info.html", recipeName=choice, detail=instructions, summary = summaries, image = images, all_ingredient = all_ingredients)
 
 
 # This handles any 404 error for the website
--- functions.py
+++ functions.py
@@ -67,11 +67,6 @@ def search_recipes_by_ingredient(ingredient):
     return list
 
 
-# Test
-recipes = search_recipes_by_ingredient(food)
-# pprint.pprint(recipes)
-
-
 def get_recipet_id(recipe_name, ingredients):
     dict = get_recipes_id_by_ingredient(ingredients)
 
@@ -96,9 +91,6 @@ def get_recipe_info(recipe_name, ingredient):
     return data
 
 
-# pprint.pprint(get_recipe_info("Easy Homemade Apple Fritters", food))
-
-
 def get_recipe_steps(recipe_name, ingredient):
     '''
     Inputs recipe name and available ingredient 
@@ -115,5 +107,75 @@ def get_recipe_steps(recipe_name, ingredient):
     return steps
 
 
-# pprint.pprint(get_recipe_steps("Easy Homemade Apple Fritters", food))
+def get_recipe_ingredients(recipe_name, ingredient):
+    '''
+    Inputs recipe name and available ingredient 
+    Returns a list of all ingredients needed and measurement in two forms
+    '''
+
+    data = get_recipe_info(recipe_name, ingredient)
+
+    ingredients_dict = {}
+
+    for instruction in data["extendedIngredients"]:
+
+        ingredients = instruction['name']
+        metric_amount = instruction['measures']['metric']['amount']
+        metric_unit = instruction['measures']['metric']['unitShort']
+        us_amount = instruction['measures']['us']['amount']
+        us_unit = instruction['measures']['us']['unitShort']
+
+        ingredients_dict[ingredients] = f"{metric_amount}{metric_unit} or {us_amount}{us_unit}"
+        
+    return ingredients_dict
+
+
+def get_recipe_summary(recipe_name, ingredient):
+    '''
+    Inputs recipe name and available ingredient 
+    Returns the summary of the recipe
+    '''
+
+    data = get_recipe_info(recipe_name, ingredient)
+
+    summary = data["summary"]
+
+    return summary
+
+
+
+def get_recipe_image(recipe_name, ingredient):
+    '''
+    Inputs recipe name and available ingredient 
+    Returns the image url of the recipe
+    '''
+
+    data = get_recipe_info(recipe_name, ingredient)
+
+    picture = data["image"]
+
+    return picture
+
+
+
+
+def main():
+    food = "apple, flour, milk"
+
+    # recipes = search_recipes_by_ingredient(food)
+    # pprint.pprint(recipes)
+
+    # pprint.pprint(get_recipe_info("Easy Homemade Apple Fritters", food))
+
+    # pprint.pprint(get_recipe_ingredients("Easy Homemade Apple Fritters", food))
+
+    # print(get_recipe_summary("Easy Homemade Apple Fritters", food))
+
+    print(get_recipe_image("Easy Homemade Apple Fritters", food))
+    
+
+if __name__ == "__main__":
+    main()
+
+
 
